e2e903cce66fbf9d1e6e1873ba313ce08f817a73
freek <freek@spatie.be>
2021-07-14 15:49:56 +0200

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          User::factory(1)->create([
-             'name' => 'Brent',
-             'email' => 'brent@spatie.be',
+             'name' => 'User',
+             'email' => 'user@example.com',
              'is_admin' => true,
          ]);
 
