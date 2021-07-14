f67daed41a4fc33758694886d960ed1d2a2a2196
freek <freek@spatie.be>
2021-07-14 17:17:33 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,33 +2,9 @@
 
 namespace Tests;
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use RefreshDatabase;
-
-    public function login(User $user = null): User
-    {
-        $user ??= User::factory()->create();
-
-        $this->actingAs($user);
-
-        return $user;
-    }
-
-    public function createRequest($method, $uri): Request
-    {
-        $symfonyRequest = SymfonyRequest::create(
-            $uri,
-            $method,
-        );
-
-        return Request::createFromBase($symfonyRequest);
-    }
 }
