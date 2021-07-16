a61881562b15c6cac4f4989b19a37bf6854a1fa9
freek <freek@spatie.be>
2021-07-16 13:53:43 +0200

    changes after video 6

--- tests/Feature/HomePageTest.php
+++ tests/Feature/HomePageTest.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\ExternalPostSuggestionController;
+use App\Mail\ExternalPostSuggestedMail;
 use App\Models\BlogPost;
+use App\Models\ExternalPost;
+use function Pest\Laravel\assertDatabaseHas;
+use \Illuminate\Support\Facades\Mail;
 
 it('can render the homepage', function () {
     $this
@@ -17,4 +22,50 @@ it('will only show published blogposts', function () {
         ->get('/')
         ->assertSee($publishedBlogPost->title)
         ->assertDontSee($draftBlogPost->title);
+});
+
+it('can accept suggestions', function () {
+    Mail::fake();
+
+    $externalPostAttributes = [
+        'title' => 'My title',
+        'url' => 'https://example.com',
+    ];
+
+    $this
+        ->post(action(ExternalPostSuggestionController::class), $externalPostAttributes)
+        ->assertSessionHasNoErrors()
+        ->assertRedirect('/');
+
+    assertDatabaseHas('external_posts', $externalPostAttributes);
+
+    Mail::assertSent(function(ExternalPostSuggestedMail $mail) use ($externalPostAttributes) {
+        if ( ! $mail->hasTo('admin@yourblog.com')) {
+            return false;
+        }
+
+        if ($mail->title !== $externalPostAttributes['title']) {
+            return false;
+        }
+
+        if ($mail->url !== $externalPostAttributes['url']) {
+            return false;
+        }
+
+        return true;
+    });
+});
+
+it('will not accept a suggestion with invalid input', function() {
+    Mail::fake();
+
+    $this
+        ->post(action(ExternalPostSuggestionController::class), [
+            'url' => 'https://example.com',
+        ])
+        ->assertSessionHasErrors(['title']);
+
+    expect(ExternalPost::count())->toBe(0);
+
+    Mail::assertNothingSent();
 });
