4372aa2e599c8152700f4c0136f870f62104df04
freek <freek@spatie.be>
2021-07-16 14:17:07 +0200

    changes after video 7

---
+++ tests/Feature/Http/Controllers/BlogPostControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\BlogPost;
+
+it('can render the homepage', function () {
+    $this
+        ->get('/')
+        ->assertSee('My Blog');
+});
+
+it('will only show published blogposts', function () {
+    $publishedBlogPost = BlogPost::factory()->published()->create();
+
+    $draftBlogPost = BlogPost::factory()->draft()->create();
+
+    $this
+        ->get('/')
+        ->assertSee($publishedBlogPost->title)
+        ->assertDontSee($draftBlogPost->title);
+});
--- tests/Feature/Http/Controllers/ExternalPostSuggestionControllerTest.php
+++ tests/Feature/Http/Controllers/ExternalPostSuggestionControllerTest.php
@@ -2,31 +2,15 @@
 
 use App\Http\Controllers\ExternalPostSuggestionController;
 use App\Mail\ExternalPostSuggestedMail;
-use App\Models\BlogPost;
 use App\Models\ExternalPost;
 use function Pest\Laravel\assertDatabaseHas;
 use Illuminate\Support\Facades\Mail;
 
-it('can render the homepage', function () {
-    $this
-        ->get('/')
-        ->assertSee('My Blog');
-});
-
-it('will only show published blogposts', function () {
-    $publishedBlogPost = BlogPost::factory()->published()->create();
-
-    $draftBlogPost = BlogPost::factory()->draft()->create();
-
-    $this
-        ->get('/')
-        ->assertSee($publishedBlogPost->title)
-        ->assertDontSee($draftBlogPost->title);
+beforeEach(function() {
+    Mail::fake();
 });
 
 it('can accept suggestions', function () {
-    Mail::fake();
-
     $externalPostAttributes = [
         'title' => 'My title',
         'url' => 'https://example.com',
@@ -57,8 +41,6 @@ it('can accept suggestions', function () {
 });
 
 it('will not accept a suggestion with invalid input', function() {
-    Mail::fake();
-
     $this
         ->post(action(ExternalPostSuggestionController::class), [
             'url' => 'https://example.com',
