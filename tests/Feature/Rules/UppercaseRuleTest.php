2a2e9229ca09f51418a99008c70cd18e559a3d4d
freek <freek@spatie.be>
2021-07-27 19:47:30 +0200

    add uppercase rule tests

---
+++ tests/Feature/Rules/UppercaseRuleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Rules\UppercaseRule;
+
+it('will pass for upper cased values', function() {
+    $result = (new UppercaseRule())->passes('name', 'MY STRING');
+    expect($result)->toBeTrue();
+
+    $result = (new UppercaseRule())->passes('name', 'my string');
+    expect($result)->toBeFalse();
+
+    $result = (new UppercaseRule())->passes('name', 'My string');
+    expect($result)->toBeFalse();
+
+    $result = (new UppercaseRule())->passes('name', 'MY string');
+    expect($result)->toBeFalse();
+
+    $result = (new UppercaseRule())->passes('name', 'MY STRINGg');
+    expect($result)->toBeFalse();
+});
