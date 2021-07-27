edb3172bc542ea2f9269f92c9581d2349c440cc2
freek <freek@spatie.be>
2021-07-27 20:03:23 +0200

    refactor to dataset

--- tests/Feature/Rules/UppercaseRuleTest.php
+++ tests/Feature/Rules/UppercaseRuleTest.php
@@ -2,19 +2,13 @@
 
 use App\Rules\UppercaseRule;
 
-it('will pass for upper cased values', function() {
-    $result = (new UppercaseRule())->passes('name', 'MY STRING');
-    expect($result)->toBeTrue();
-
-    $result = (new UppercaseRule())->passes('name', 'my string');
-    expect($result)->toBeFalse();
-
-    $result = (new UppercaseRule())->passes('name', 'My string');
-    expect($result)->toBeFalse();
-
-    $result = (new UppercaseRule())->passes('name', 'MY string');
-    expect($result)->toBeFalse();
-
-    $result = (new UppercaseRule())->passes('name', 'MY STRINg');
-    expect($result)->toBeFalse();
-});
+it('will pass for upper cased values', function(string $value, bool $expectedResult) {
+    $result = (new UppercaseRule())->passes('name', $value);
+    expect($result)->toBe($expectedResult);
+})->with([
+    ['MY STRING', true],
+    ['my string', false],
+    ['My string', false],
+    ['MY string', false],
+    ['MY STRINg', false],
+]);
