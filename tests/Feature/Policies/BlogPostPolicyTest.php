f2cb76e284c355c5a8b7c47c3750cdbd6e7d9382
freek <freek@spatie.be>
2021-07-28 11:17:05 +0200

    refactor policy test

--- tests/Feature/Policies/BlogPostPolicyTest.php
+++ tests/Feature/Policies/BlogPostPolicyTest.php
@@ -8,39 +8,42 @@ use App\Models\User;
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
 
-it('will allow admin users to manage blog posts', function () {
+it('will allow admin users to manage blog posts', function (Closure $request) {
     $post = BlogPost::factory()->create();
 
     $admin = User::factory()->admin()->create();
 
     login($admin);
 
-    get(action([BlogPostAdminController::class, 'index']))->assertSuccessful();
-    get(action([BlogPostAdminController::class, 'create']))->assertSuccessful();
-    post(action([BlogPostAdminController::class, 'store']))->assertRedirect();
-    get(action([BlogPostAdminController::class, 'edit'], $post->slug))->assertSuccessful();
-    post(action([BlogPostAdminController::class, 'update'], $post->slug))->assertRedirect();
-    post(action([BlogPostAdminController::class, 'publish'], $post->slug))->assertRedirect();
-    post(action(UpdatePostSlugController::class, $post->slug))->assertRedirect();
-    post(action(DeletePostController::class, $post->slug))->assertRedirect();
-});
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $request($post);
 
-it('will not allow guest users to manage blog posts', function () {
+    expect($response->status())->toBeIn([200, 301, 302]);
+})->with('requests');
+
+it('will not allow guest users to manage blog posts', function (Closure $request) {
     $post = BlogPost::factory()->create();
 
     $guest = User::factory()->guest()->create();
 
     login($guest);
 
-    get(action([BlogPostAdminController::class, 'index']))->assertForbidden();
-    get(action([BlogPostAdminController::class, 'create']))->assertForbidden();
-    post(action([BlogPostAdminController::class, 'store']))->assertForbidden();
-    get(action([BlogPostAdminController::class, 'edit'], $post->slug))->assertForbidden();
-    post(action([BlogPostAdminController::class, 'update'], $post->slug))->assertForbidden();
-    post(action([BlogPostAdminController::class, 'publish'], $post->slug))->assertForbidden();
-    post(action(UpdatePostSlugController::class, $post->slug))->assertForbidden();
-    post(action(DeletePostController::class, $post->slug))->assertForbidden();
-});
+    /** @var \Illuminate\Testing\TestResponse $response */
+    $response = $request($post);
+
+    expect($response->isForbidden())->toBeTrue();
+})->with('requests');
+
+dataset('requests', [
+    fn(BlogPost $post) => get(action([BlogPostAdminController::class, 'index'])),
+    fn(BlogPost $post) => get(action([BlogPostAdminController::class, 'create'])),
+    fn(BlogPost $post) => post(action([BlogPostAdminController::class, 'store'])),
+    fn(BlogPost $post) => get(action([BlogPostAdminController::class, 'edit'], $post->slug)),
+    fn(BlogPost $post) => post(action([BlogPostAdminController::class, 'update'], $post->slug)),
+    fn(BlogPost $post) => post(action([BlogPostAdminController::class, 'publish'], $post->slug)),
+    fn(BlogPost $post) => post(action(UpdatePostSlugController::class, $post->slug)),
+    fn(BlogPost $post) => post(action(DeletePostController::class, $post->slug)),
+]);
 
 
 
