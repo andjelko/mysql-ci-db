4f5d7876bffec1fb9f2ebef69523a68a59310770
freek <freek@spatie.be>
2021-08-06 10:21:12 +0200

    wip

--- tests/Feature/Http/Controllers/BlogPostAdminControllerTest.php
+++ tests/Feature/Http/Controllers/BlogPostAdminControllerTest.php
@@ -3,20 +3,20 @@
 use App\Http\Controllers\BlogPostAdminController;
 use App\Models\BlogPost;
 use App\Models\User;
+use Tests\Factories\BlogPostRequestDataFactory;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\post;
 
-beforeEach(function() {
+beforeEach(function () {
     $this->post = BlogPost::factory()->create();
+    $this->requestData = BlogPostRequestDataFactory::new();
 });
 
 it('will update a blog post when an admin is logged in', function () {
-    $sendRequest = fn() => post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [
-        'title' => 'test',
-        'author' => $this->post->author,
-        'body' => $this->post->body,
-        'date' => $this->post->date->format('Y-m-d'),
-    ]);
+    $sendRequest = fn() => post(
+        action([BlogPostAdminController::class, 'update'], $this->post->slug),
+        $this->requestData->withTitle('test')->create(),
+    );
 
     $sendRequest()->assertRedirect(route('login'));
 
@@ -33,23 +33,19 @@ it('validates required fields on the post edit form', function () {
     post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [])
         ->assertSessionHasErrors(['title', 'author', 'body', 'date']);
 
-    post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [
-        'title' => 'test',
-        'author' => $this->post->author,
-        'body' => $this->post->body,
-        'date' => $this->post->date->format('Y-m-d'),
-    ])->assertSessionHasNoErrors();
+    post(
+        action([BlogPostAdminController::class, 'update'], $this->post->slug),
+        $this->requestData->withTitle('test')->create()
+    )->assertSessionHasNoErrors();
 });
 
-it('will validate the date format on the post edit form', function() {
+it('will validate the date format on the post edit form', function () {
     login();
 
-    post(action([BlogPostAdminController::class, 'update'], $this->post->slug), [
-        'title' => $this->post->title,
-        'author' => $this->post->author,
-        'body' => $this->post->body,
-        'date' => '01/01/2021',
-    ])->assertSessionHasErrors([
+    post(
+        action([BlogPostAdminController::class, 'update'], $this->post->slug),
+        $this->requestData->withDate('01/01/2021')->create(),
+    )->assertSessionHasErrors([
         'date' => 'The date does not match the format Y-m-d.'
     ]);
 });
