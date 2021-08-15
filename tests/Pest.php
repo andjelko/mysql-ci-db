ad4d5ff318166b29cff027cf0c0ef9e0b5bbfb61
freek <freek@spatie.be>
2021-08-15 12:17:16 +0200

    add get feed function, lesson starts now

--- tests/Pest.php
+++ tests/Pest.php
@@ -27,6 +27,50 @@ function createRequest($method, $uri): Request
     return Request::createFromBase($symfonyRequest);
 }
 
+function getFeed(string $title = 'test'): string
+{
+    return  <<<XML
+       <feed xmlns="http://www.w3.org/2005/Atom">
+           <id>https://test.com/rss</id>
+           <link href="https://test.com/rss"/>
+           <title><![CDATA[ https://test.com ]]></title>
+           <updated>2021-08-11T11:00:30+00:00</updated>
+
+           <entry>
+               <title><![CDATA[$title]]></title>
+
+               <link rel="alternate" href="https://test.com/blog/test"/>
+
+               <id>https://test.com/blog/test</id>
+
+               <author>
+                   <name><![CDATA[ Brent Roose ]]></name>
+               </author>
+
+               <summary type="html"><![CDATA[$title]]></summary>
+
+               <updated>2021-07-29T00:00:00+00:00</updated>
+           </entry>
+
+           <entry>
+               <title><![CDATA[$title]]></title>
+
+               <link rel="alternate" href="https://test.com/blog/test"/>
+
+               <id>https://test.com/blog/test</id>
+
+               <author>
+                   <name><![CDATA[ Brent Roose ]]></name>
+               </author>
+
+               <summary type="html"><![CDATA[$title]]></summary>
+
+               <updated>2021-07-29T00:00:00+00:00</updated>
+           </entry>
+       </feed>
+       XML;
+}
+
 expect()->extend('isForbidden', function() {
     return expect($this->value->isForbidden())->toBeTrue();
 });
