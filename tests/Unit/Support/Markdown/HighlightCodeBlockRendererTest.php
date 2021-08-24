9b44872804395e4a0833dd0f02aed0a6f41fe3e3
freek <freek@spatie.be>
2021-08-24 10:33:45 +0200

    wip

---
+++ tests/Unit/Support/Markdown/HighlightCodeBlockRendererTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use League\CommonMark\MarkdownConverter;
+use function Spatie\Snapshots\assertMatchesHtmlSnapshot;
+
+it('can parse hljs tags', function () {
+    $markdown = <<<MD
+```php
+public function __construct(
+    <hljs keyword>public readonly</hljs> <hljs type>string</hljs> <hljs prop>\$title</hljs>,
+    <hljs keyword>public readonly</hljs> <hljs type>string</hljs> <hljs prop>\$body</hljs>,
+) {}
+```
+MD;
+
+    $convertor = app(MarkdownConverter::class);
+
+    $html = $convertor->convertToHtml($markdown);
+
+    assertMatchesHtmlSnapshot($html);
+});
